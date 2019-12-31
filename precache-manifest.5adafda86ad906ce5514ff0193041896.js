39bddbc14ecc1ba8d774aea984bd1e386f21c304
ileung6 <simonclaudia100@gmail.com>
2019-12-30 16:10:59 -0800

    Updates

---
+++ precache-manifest.5adafda86ad906ce5514ff0193041896.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12e191750088513836a405d8a22c9118",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3f4e7fe78186c6814caf",
+    "url": "/portfolio/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9d10024a7984f539a5a2",
+    "url": "/portfolio/static/css/main.9bc9f9c3.chunk.css"
+  },
+  {
+    "revision": "3f4e7fe78186c6814caf",
+    "url": "/portfolio/static/js/2.9bddaf49.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/portfolio/static/js/2.9bddaf49.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9d10024a7984f539a5a2",
+    "url": "/portfolio/static/js/main.5324df99.chunk.js"
+  },
+  {
+    "revision": "8e33e263dd9f3af2ec21",
+    "url": "/portfolio/static/js/runtime-main.c6ae4aa1.js"
+  },
+  {
+    "revision": "954674522c5e83a6af9676cb6bb2fa81",
+    "url": "/portfolio/static/media/Buildings.95467452.png"
+  },
+  {
+    "revision": "01cd9e50cfe63d5001c3c8282b5d5ea1",
+    "url": "/portfolio/static/media/Hey I'm Ivan.01cd9e50.png"
+  },
+  {
+    "revision": "6426ddbc52c25ca298a4b42fb5ecbf14",
+    "url": "/portfolio/static/media/Me.6426ddbc.png"
+  },
+  {
+    "revision": "59bd97b45b63da0e325282ac29008d24",
+    "url": "/portfolio/static/media/Skills.59bd97b4.png"
+  }
+]);
